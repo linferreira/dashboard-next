fd7adcc8062b91c731453d9ed6508239a31d7849
Lindsay Ferreira <lindsayferreira@ext.cvccorp.com.br>
2023-01-06 11:34:28 -0300

    style: create a theme provider

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,16 @@
-import type { AppProps } from 'next/app'
+import type { AppProps } from "next/app";
+import { GlobalStyles } from "@/styles/globalStyles";
+import { Global, ThemeProvider } from "@emotion/react";
+import { dark } from "@/styles/themes";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+
+  return (
+    <ThemeProvider theme={dark}>
+      <Global styles={GlobalStyles} />
+      <Component {...pageProps} />
+    </ThemeProvider>
+  );
 }
 
-export default MyApp
+export default MyApp;
